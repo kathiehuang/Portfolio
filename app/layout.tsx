07fbab77d19a8ab70a591c63bc450632d5162f7d
kathiehuang <46662481+kathiehuang@users.noreply.github.com>
2023-06-12 16:08:31 -0700

    added nav bar

---
+++ app/layout.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import './globals.css'
+import { Inter } from 'next/font/google'
+import Navbar from './navbar/navbar'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata = {
+  title: 'Kathie Huang',
+  description: 'Personal Portfolio Website',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+      <html lang="en">
+        <body className={inter.className}>{children}</body>
+      </html>
+  )
+}
