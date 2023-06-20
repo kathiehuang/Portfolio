27b98dc83fa77bf0f87c4182b1b3777a4840c979
Kathie Huang <kathie.huang@duke.edu>
2023-06-19 17:34:24 -0700

    added metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,29 @@
 import React from 'react'
 import './globals.css'
 import { Inter } from 'next/font/google'
-import Navbar from './components/navbar'
+import { Metadata } from 'next'
+
+export const metadata: Metadata = {
+  title: 'Kathie Huang',
+  description: 'Kathie Huang is a third-year college student at Duke University studying computer science. Currently interning at Warner Bros. Discovery.',
+  keywords: ['Kathie Huang', 'Software Engineer', 'Portfolio', 'Duke University', 'Warner Bros. Discovery', 'Ford Motor Company', 'Computer Science Student'],
+  twitter: {
+    card: 'summary_large_image',
+    title: 'Kathie Huang',
+    description: 'Kathie Huang is a third-year college student at Duke University studying computer science. Currently interning at Warner Bros. Discovery.',
+    creator: '@KathieHuang',
+    images: {
+      url: 'icon.jpeg',
+      alt: 'Kathie Huang Photo'
+    },
+  },
+  appleWebApp: {
+    title: 'Kathie Huang',
+    statusBarStyle: 'black-translucent',
+  },
+}
 
 const inter = Inter({ subsets: ['latin'] })
-
-export const metadata = {
-  title: 'Kathie Huang',
-  description: 'Personal Portfolio Website',
-}
 
 export default function RootLayout({
   children,
@@ -16,8 +31,10 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-      <html lang="en">
+    <html lang="en">
+      <head>
         <body className={inter.className}>{children}</body>
-      </html>
+      </head>
+    </html>
   )
 }
